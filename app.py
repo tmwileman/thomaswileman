c6578ac9b70c5ba8509e9790e6868b5af1a941c0
twileman <twileman@redhat.com>
2021-04-02 22:14:10 -0400

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+import csv
+from flask import Flask, render_template, request, redirect
+
+
+app = Flask(__name__)
+app.config["SEND_FILE_MAX_AGE_DEFAULT"] = 0
+
+# Home page
+@app.route("/", methods=["GET", "POST"])
+def home():
+    return render_template("home.html")
+
+
+@app.route("/notes", methods=["GET", "POST"])
+def notes():
+    return render_template("notes.html")
+
+
+@app.route("/eli5", methods=["GET", "POST"])
+def eli5():
+    return render_template("eli5.html")
+
+
+@app.route("/about", methods=["GET", "POST"])
+def about():
+    return render_template("about.html")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
